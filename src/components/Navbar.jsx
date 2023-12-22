e369d79e3839000d089a6c799840b68c6cbe9143
Audi <audi.previo98@gmail.com>
2023-12-22 20:51:49 +0700

    fix: navbar fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,19 +19,16 @@ const Navbar = () => {
 
   useEffect(() => {
     const fetchProfile = async () => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
       try {
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_OKSIGEN_API_BASE_URL}/profile`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_OKSIGEN_API_BASE_URL}/profile`, {
+          headers: {
+            'Authorization': `Bearer ${token}`,
+          },
+        });
         setProfile(response.data.data);
       } catch (error) {
-        console.error("Error fetching profile:", error);
+        console.error('Error fetching profile:', error);
       }
     };
 
@@ -73,48 +70,35 @@ const Navbar = () => {
     <div className="bg-white  py-4 pl-8 pr-4 flex flex-row items-center justify-between relative z-1000">
       <div className="flex flex-row justify-center items-center">
         <Image src={LogoOksigen} alt="logo oksigen" className="mr-10" />
-        <Button
-          variant="light"
-          onClick={handleOksigenMapClick}
-          className="px-8 !text-sm font-medium text-oksigen-brand-blackX hover:text-oksigen-brand-blue !bg-none"
-        >
+        <Button variant="light" onClick={handleOksigenMapClick} className="px-8 !text-sm font-medium text-oksigen-brand-blackX hover:text-oksigen-brand-blue !bg-none">
           OksigenMap
         </Button>
-        <Button
-          variant="light"
-          onClick={handleSubscriptionClick}
-          className="px-8 !text-sm font-medium text-oksigen-brand-blackX hover:text-oksigen-brand-blue !bg-none"
-        >
+        <Button variant="light" onClick={handleSubscriptionClick} className="px-8 !text-sm font-medium text-oksigen-brand-blackX hover:text-oksigen-brand-blue !bg-none">
           Langganan OksigenPlus
         </Button>
       </div>
       <div className="flex flex-row justify-center items-start">
-        <div
-          className="flex items-end flex-col"
-          onClick={toggleCardVisibility}
-          style={{ cursor: "pointer" }}
-        >
-          <Title
-            className="!text-sm !font-semibold text-oksigen-brand-blackX mr-2
+      <div
+        className="flex items-end flex-col"
+        onClick={toggleCardVisibility}
+        style={{ cursor: "pointer" }}
+      >
+        <Title
+          className="!text-sm !font-semibold text-oksigen-brand-blackX mr-2
  hover:bg-oksigen-brand-blue hover:bg-opacity-5 
  hover:py-2 hover:px-4 px-4 py-2 hover:rounded-tremor-full transition-all duration-300 hover:text-oksigen-brand-blue"
-          >
-            {profile?.isPremium && (
-              <Badge size="md" className="mr-4 font-normal">
-                OksigenPlus
-              </Badge>
-            )}{" "}
-            <b className="font-semibold">Halo, {profile?.organization_name}!</b>
-          </Title>
-        </div>
+        >
+         {profile?.isPremium && <Badge size="md" className="mr-4 font-normal">OksigenPlus</Badge>} <b className="font-semibold">Halo, {profile?.organization_name}!</b> 
+        </Title>
+      </div>
         {isCardVisible && (
           <div className="absolute top-full right-8 mt-2 items-">
             <Card className="!rounded-3xl flex items-end justify-end flex-col gap-">
-              <div onClick={() => router.push("/profile-edit")}>
-                <Text className="!text-oksigen-brand-blackX !hover:text-oksigen-brand-blue cursor-pointer">
-                  Atur Profil
-                </Text>
-              </div>
+    <div onClick={() => router.push('/profile-edit')}>
+      <Text className="!text-oksigen-brand-blackX !hover:text-oksigen-brand-blue cursor-pointer">
+        Atur Profil
+      </Text>
+    </div>
               <div onClick={logout}>
                 <Text className="mt-4 cursor-pointer">Keluar</Text>
               </div>
